fe9c2cf4fe4f0d28fffcfacb189309898851fcb0
Eric Chen <echen0719@gmail.com>
2025-04-05 13:03:17 -0400

    Update athlete_scraper.py

--- athlete_scraper.py
+++ athlete_scraper.py
@@ -18,6 +18,8 @@ def getLevels(athlete):
                 if heading not in levels and heading != 'College Progression Tracker': # short-circuit, I think
                     levels.append(heading)
 
+        return levels
+
     elif response.status_code == 404:
         return # some profiles don't exist like https://www.milesplit.com/athletes/{9 through 14}
 
@@ -25,14 +27,12 @@ def getLevels(athlete):
         print(f"Try Again. Something wrong. Status Code: {response.status_code}")
         exit()
 
-    return levels
-
 def getNames(athlete):
     response = requests.get(athlete)
 
     if response.status_code == 200:
        soup = BeautifulSoup(response.text, 'html.parser')
-       name = soup.find('h1', {'id': 'athleteName'}).get_text(strip=True)
+       return soup.find('h1', {'id': 'athleteName'}).get_text(strip=True)
 
     elif response.status_code == 404:
         return
@@ -40,24 +40,25 @@ def getNames(athlete):
     else:
         print(f"Try Again. Something wrong. Status Code: {response.status_code}")
         exit()
-        
-    return name
 
 def main():
-    for i in range(1, 16268836): # currents max athlete MS has
-        athlete = "https://www.milesplit.com/athletes/{}".format(i)
-        print(getLevels(athlete))
+    with open("all-athlete-names.txt", mode='a') as file:
+        for i in range(1, 16268836): # currents max athlete MS has
+            athlete = "https://www.milesplit.com/athletes/{}".format(i)
+            name = getNames(athlete)
+            if name:
+                file.write(str(i) + ". " + name + "\n")
 
 if __name__ == "__main__":
-    #main()
-    pass
+    main()
+    # pass
 
-print(getLevels("https://www.milesplit.com/athletes/13822077"))
+# print(getLevels("https://www.milesplit.com/athletes/13822077"))
 # test athlete 1 would return ['2025 Indoor Rankings', '2024 Indoor Rankings', 'High School']
-print(getLevels("https://www.milesplit.com/athletes/15218721"))
+# print(getLevels("https://www.milesplit.com/athletes/15218721"))
 # test athlete 2 (me) would return ['High School']
 
-print(getNames("https://www.milesplit.com/athletes/13822077"))
+# print(getNames("https://www.milesplit.com/athletes/13822077"))
 # test athlete 1 would return 'Raghu Moorthy'
-print(getNames("https://www.milesplit.com/athletes/15218721"))
+# print(getNames("https://www.milesplit.com/athletes/15218721"))
 # test athlete 2 would return 'Eric Chen'
